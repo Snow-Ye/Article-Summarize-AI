b67fcb8cf33b57cc2658fe2ed0a1c2a7725248cd
CH Ye <w17415157@gmail.com>
2023-04-24 17:05:47 -0400

    Final

---
+++ src/services/article.js
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+
+export const articleApi = createApi({
+  reducerPath: "articleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "38e0d4a681msh9f7c2f5b5bf76e9p1356dcjsnda4b15c25598"
+      );
+      headers.set(
+        "X-RapidAPI-Host",
+        "article-extractor-and-summarizer.p.rapidapi.com"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getSummary: builder.query({
+      query: (params) =>
+        `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+    }),
+  }),
+});
+
+export const { useLazyGetSummaryQuery } = articleApi;
